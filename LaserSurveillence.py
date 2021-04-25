c851be51d3b0c62ead3c579846fbbb612e3d1aff
Jaeltz978 <81648561+Jaeltz978@users.noreply.github.com>
2021-04-25 02:32:10 -0400

    Delete LaserSurveillence.py

--- LaserSurveillence.py
+++
@@ -1,9 +0,0 @@
-from gpiozero import LightSensor
-from time import sleep
-import requests # idea from https://thepihut.com/blogs/raspberry-pi-tutorials/using-ifttt-with-the-raspberry-pi
-ldr = LightSensor(4)
-
-while True:
-    sleep(0.3) # link to found
-    if ldr.value < 0.5:
-        http://requests.post('https://maker.ifttt.com/trigger/Laser_trip/with/key/noB6nShqQRFuBOW_CcodJlSVBC7wUWNZQhUi60078lT')
